5c4eac45767d293a628e80c4bc49cee7f21ca6fd
Eric Stephenson <ericb.stephenson@gmail.com>
2016-01-24 14:03:15 -0800

    Level 3 Complete

---
+++ js/app.js
@@ -0,0 +1,110 @@
+var App = Ember.Application.create({
+  LOG_TRANSITIONS: true
+});
+
+App.Router.map(function() {
+  this.route('credits', { path: '/thanks' });
+  this.resource('products', function() {
+    this.resource('product', { path: '/:product_id' });
+  });
+  this.resource('contacts', function() {
+    this.resource('contact', { path: '/:name' });
+  });
+});
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: 'images/logo.png',
+  time: function() {
+  	return (new Date()).toDateString();
+  }.property()
+});
+
+App.ContactsIndexController = Ember.Controller.extend({
+  contactName: 'Anostagia',
+  avatar: 'images/avatar.png',
+  open: function() {
+  	var day = (new Date()).getDay();
+  	if (day === 0) {
+  	  return 'We are closed on Sunday :(';
+  	} else {
+  	  return 'We are open today!';
+  	}
+  }.property()
+});
+
+App.Product = DS.Model.extend({
+  title: DS.attr('sting'),
+  price: DS.attr('number'),
+  description: DS.attr('sting'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('sting')
+});
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+  	return this.store.findAll('product');
+  }
+});
+
+App.ContactsRoute = Ember.Route.extend({
+  model: function() {
+    return App.CONTACTS;
+  }
+});
+
+App.ContactRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.CONTACTS.findBy('name', params.name);
+  }
+});
+
+App.Product.FIXTURES = [
+  {
+  	id: 1,
+  	title: 'Flint',
+  	price: 99,
+  	description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+  	isOnSale: true,
+  	image: 'images/products/flint.png'
+  },
+  {
+  	id: 2,
+  	title: 'Kindling',
+  	price: 249,
+  	description: 'Easily combustible small sticks or twigs used for starting a fire.',
+  	isOnSale: false,
+  	image: 'images/products/kindling.png'
+  }
+];
+
+App.CONTACTS = [
+  {
+    name: 'Giamia',
+    about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
+    avatar: 'images/contacts/giamia.png'
+  },
+  {
+    name: 'Anostagia',
+    about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
+    avatar: 'images/contacts/anostagia.png'
+  }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
