3f33906ee8e8f5b8c6f587e344069f97414b6cb5
Eric Stephenson <ericb.stephenson@gmail.com>
2016-01-24 15:42:02 -0800

    Level 4 Complete

--- js/app.js
+++ js/app.js
@@ -52,7 +52,9 @@ App.Product = DS.Model.extend({
   price: DS.attr('number'),
   description: DS.attr('sting'),
   isOnSale: DS.attr('boolean'),
-  image: DS.attr('sting')
+  image: DS.attr('sting'),
+  reviews: DS.hasMany('review', {async: true}),
+  crafter: DS.belongsTo('contact', {async: true})
 });
 
 App.Product.FIXTURES = [
@@ -62,7 +64,9 @@ App.Product.FIXTURES = [
   	price: 99,
   	description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
   	isOnSale: true,
-  	image: 'images/products/flint.png'
+  	image: 'images/products/flint.png',
+  	reviews: [100, 101],
+  	crafter: 1
   },
   {
   	id: 2,
@@ -70,14 +74,36 @@ App.Product.FIXTURES = [
   	price: 249,
   	description: 'Easily combustible small sticks or twigs used for starting a fire.',
   	isOnSale: false,
-  	image: 'images/products/kindling.png'
+  	image: 'images/products/kindling.png',
+  	reviews: [],
+  	crafter: 2
+  }
+];
+
+App.Review = DS.Model.extend({
+  text: DS.attr('string'),
+  reviewedAt: DS.attr('date'),
+  product: DS.belongsTo('product')
+});
+
+App.Review.FIXTURES = [
+  {
+  	id: 100,
+  	product: 1,
+  	text: 'Started a fire in no time!'
+  },
+  {
+  	id: 101,
+  	product: 1,
+  	text: 'Not the brightest flame, but warm!'
   }
 ];
 
 App.Contact = DS.Model.extend({
   name: DS.attr('string'),
   about: DS.attr('string'),
-  avatar: DS.attr('string')
+  avatar: DS.attr('string'),
+  products: DS.hasMany('product', {async: true})
 });
 
 App.Contact.FIXTURES = [
@@ -85,15 +111,19 @@ App.Contact.FIXTURES = [
   	id: 1,
     name: 'Giamia',
     about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
-    avatar: 'images/contacts/giamia.png'
+    avatar: 'images/contacts/giamia.png',
+    products: [1]
   },
   {
   	id: 2,
     name: 'Anostagia',
     about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
-    avatar: 'images/contacts/anostagia.png'
+    avatar: 'images/contacts/anostagia.png',
+    products: [2]
   }
 ];
+
+
 
 
 
